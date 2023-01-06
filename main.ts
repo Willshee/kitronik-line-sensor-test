e8488238e982d322e65d63737000e4fe47a2f25d
Willshee <77567684+Willshee@users.noreply.github.com>
2023-01-06 22:35:42 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,69 @@
+input.onGesture(Gesture.ScreenDown, function () {
+    running = 0
+    Kitronik_Move_Motor.stop()
+    basic.showLeds(`
+        . # # # .
+        # # . . #
+        # . # . #
+        # . . # #
+        . # # # .
+        `)
+    basic.pause(1000)
+    Kitronik_Move_Motor.stop()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    running = 1
+})
+let difference = 0
 let rightSens = 0
 let leftSens = 0
-let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+let running = 0
+let speed = 0
+Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Tight)
+basic.showLeds(`
+    . # # # .
+    # # . . #
+    # . # . #
+    # . . # #
+    . # # # .
+    `)
 basic.forever(function () {
-    leftSens = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
-    rightSens = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
-    serial.writeValue("left", leftSens)
-    serial.writeValue("right", rightSens)
-    leftSens = pins.map(
-    leftSens,
-    150,
-    220,
-    0,
-    255
-    )
-    rightSens = pins.map(
-    rightSens,
-    120,
-    200,
-    0,
-    255
-    )
-    moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.rgb(leftSens, leftSens, leftSens))
-    moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.rgb(rightSens, rightSens, rightSens))
-    moveMotorZIP.show()
+    if (running) {
+        leftSens = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+        rightSens = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+        serial.writeValue("left", leftSens)
+        serial.writeValue("right", rightSens)
+        difference = Math.abs(leftSens - rightSens)
+        serial.writeValue("diff", difference)
+        if (difference > 10) {
+            if (leftSens < rightSens) {
+                Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Left, speed)
+                basic.showLeds(`
+                    . . # . .
+                    . . . # .
+                    . . . . #
+                    . . . # .
+                    . . # . .
+                    `)
+            } else {
+                Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, speed)
+                basic.showLeds(`
+                    . . # . .
+                    . # . . .
+                    # . . . .
+                    . # . . .
+                    . . # . .
+                    `)
+            }
+        } else {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+            basic.showLeds(`
+                . . # . .
+                . # . # .
+                # . . . #
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
 })
